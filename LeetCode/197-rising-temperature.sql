88949b76d03cac70e3a196b8675445c535ae03b7
Adarshini Azeka <adarshiniazeka@gmail.com>
2026-09-04 19:24:01 +0530

    Add LeetCode 197 solution
    Add SQL query for LeetCode problem 197 to find rising temperatures.

---
+++ LeetCode/197-rising-temperature.sql
@@ -0,0 +1,8 @@
+---Leetcode 197
+---Difficulty:easy
+---Concepts: self join, DATEDIFF(), WHERE clause
+
+select w1.id from Weather w1
+join Weather w2 on
+datediff(w1.recordDate,w2.recordDate)=1
+where w1.temperature>w2.temperature;
