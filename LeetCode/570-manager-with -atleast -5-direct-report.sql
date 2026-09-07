4a69ce2795e748d41246a8338014b87bd2a396c4
Adarshini Azeka <adarshiniazeka@gmail.com>
2026-09-07 18:38:54 +0530

    Add LeetCode 570 solution

--- LeetCode/570-manager-with -atleast -5-direct-report.sql
+++ LeetCode/570-manager-with -atleast -5-direct-report.sql
@@ -8,4 +8,14 @@ WHERE id IN (
     SELECT managerId 
     FROM Employee 
     GROUP BY managerId 
-    HAVING COUNT(*) >= 5)
+    HAVING COUNT(*) >= 5);
+
+    
+---WITHOUT SUBQUERY
+---Using SELF JOIN
+    
+select e1.name from Employee e1
+inner join Employee e2
+on e1.id=e2.managerId
+group by e2.managerId
+having count(e2.managerId)>=5;
