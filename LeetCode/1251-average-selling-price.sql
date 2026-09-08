3edbf996cfd8d5c156a62b538859916d4c641444
Adarshini Azeka <adarshiniazeka@gmail.com>
2026-09-08 20:42:06 +0530

    Add LeetCode 1251 solution

---
+++ LeetCode/1251-average-selling-price.sql
@@ -0,0 +1,11 @@
+---Leetcode 1251
+---Dificulty: easy
+---Concepts: IFNULL, JOINS, ROUND(), Aggregate functions[sum()], AND and BETWEEN opertors, GROUPBY
+
+select p.product_id,
+ifnull(round(sum(units*price)/sum(units),2),0.00) as average_price from Prices p
+left join UnitsSold u on
+p.product_id=u.product_id
+and u.purchase_date between
+p.start_date and p.end_date
+group by p.product_id;
